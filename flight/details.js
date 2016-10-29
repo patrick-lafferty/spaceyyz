8f656d1d23f03ddae6998bb15a6106d480a804cf
Patrick Lafferty <patrick_lafferty@outlook.com>
2016-10-28 20:08:58 -0400

    [flight] started designing realtime flight display that shows an ascent path and key event log when events happen

--- flight/details.js
+++ flight/details.js
@@ -12,15 +12,27 @@
 		var self = this;
 		this.flight = $stateParams.flight;
 
+		function checkLaunchStatus() {
+			if (self.flight.launch.date <= Date.now) {
+				self.flight.launched = true;
+			}
+		}
+
 		if (typeof this.flight.mission === "undefined") {
 			//possibly refreshed the page, see if we can pull up the order from the db
 			flightFactory.getFlight($stateParams.missionName).then(function (flight) {
 				self.flight = flight;
 				
+				self.flight.launched = false;
+				checkLaunchStatus();
+
 				$timeout(function() {
 					$scope.$apply();
 				});
 			});
+		} else {
+			self.flight.launched = false;
+			checkLaunchStatus();
 		}
 
 	}
