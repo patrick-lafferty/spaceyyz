945254307404940b931b3f2f3fd41f6bfc86c7b2
Patrick Lafferty <patrick_lafferty@outlook.com>
2016-10-31 11:59:18 -0400

    [flight] added points on the ascent path representing key flight events, point and label change colour when the path reaches that point, playback speed is user controllable

--- flight/details.js
+++ flight/details.js
@@ -11,6 +11,7 @@
 	function FlightDetails($stateParams, flightFactory, $timeout, $scope) {
 		var self = this;
 		this.flight = $stateParams.flight;
+		this.playbackSpeed = "Realtime";
 
 		function checkLaunchStatus() {
 			if (self.flight.launch.date <= Date.now) {
@@ -35,6 +36,51 @@
 			checkLaunchStatus();
 		}
 
+		var path = document.getElementById("ascentPath");
+		var point = document.getElementById("point");
+
+		var svg = document.querySelector("svg");
+		var p = svg.createSVGPoint();
+
+		path.addEventListener("mousedown", function (event) {
+			//console.log(event.x + ", " + event.y);
+			p.x = event.clientX;
+			p.y = event.clientY;
+			var f = p.matrixTransform(svg.getScreenCTM().inverse());
+	
+			point.setAttribute("cx", f.x);
+			point.setAttribute("cy", f.y);
+
+			console.log(f.x + ", " + f.y);
+		});
+
+		
+
+		self.animLength = 5.0;
+		var lengths = [0.022, 0.167, 0.4, 0.55, 0.954];
+		this.changePlaybackSpeed = function () {
+			switch(self.playbackSpeed) {
+				case "Realtime": {
+					self.animLength = 600;
+					break;
+				}
+				case "10x": {
+					self.animLength = 60;
+					break;
+				}
+			}
+
+			document.querySelectorAll("circle.futureFlightEvent").forEach(function (e, i) {
+				e.style.setProperty('animation-delay', lengths[i] * self.animLength + 's');
+			});
+			document.querySelectorAll("text.futureFlightEvent").forEach(function (e, i) {
+				e.style.setProperty('animation-delay', lengths[i] * self.animLength + 's');
+			});
+
+			path.style.setProperty('animation-duration', self.animLength + 's');
+		};
+
+		this.changePlaybackSpeed();
 	}
 
 })();
